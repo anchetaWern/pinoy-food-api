036094d097f4999bcde6f10b3d4b5187565ce722
Wern Ancheta <ancheta.wern@gmail.com>
2024-07-06 19:05:21 +0800

    [improvement] show image in uploads review page

--- app/Http/Controllers/FoodLabelUploadListController.php
+++ app/Http/Controllers/FoodLabelUploadListController.php
@@ -20,19 +20,25 @@ class FoodLabelUploadListController extends Controller
 
         return Datatables::of($food_uploads)
             ->editColumn('title_image', function ($model) {
-                return '<button class="btn btn-secondary btn-sm view-image" data-title="Title/Food Image" data-url="' . url('storage/' . $model->title_image) . '">View</button>';
+                return '<img src="' . url('storage/' . $model->title_image) . '" class="view-image small-img" data-title="Title/Food Image" data-url="' . url('storage/' . $model->title_image) . '" />';
             })
             ->editColumn('nutrition_label_image', function ($model) {
                 $nutrition_label_image = ($model->nutrition_label_image) ? url('storage/' . $model->nutrition_label_image) : '';
-                return '<button class="btn btn-secondary btn-sm view-image" data-title="Nutrition label Image" data-url="' . $nutrition_label_image . '">View</button>';
+                if ($nutrition_label_image) {
+                    return '<img src="' . url('storage/' . $model->nutrition_label_image) . '" class="view-image small-img" data-title="Nutrition label Image" data-url="' . $nutrition_label_image . '" />';
+                }
             })
             ->editColumn('ingredients_image', function ($model) {
                 $ingredients_image = ($model->ingredients_image) ? url('storage/' . $model->ingredients_image) : '';
-                return '<button class="btn btn-secondary btn-sm view-image" data-title="Ingredients Image" data-url="' . $ingredients_image . '">View</button>';
+                if ($ingredients_image) {
+                    return '<img src="' . url('storage/' . $model->ingredients_image) . '" class="view-image small-img" data-title="Ingredients Image" data-url="' . $ingredients_image . '" />';
+                }
             })
             ->editColumn('barcode_image', function ($model) {
                 $barcode_image = ($model->barcode_image) ? url('storage/' . $model->barcode_image) : '';
-                return '<button class="btn btn-secondary btn-sm view-image" data-title="Barcode Image" data-url="' . $barcode_image . '">View</button>';
+                if ($barcode_image) {
+                    return '<img src="' . url('storage/' . $model->barcode_image) . '" class="view-image small-img" data-title="Barcode Image" data-url="' . $barcode_image . '" />';
+                }
             })
 
             ->editColumn('id', function ($model) {
