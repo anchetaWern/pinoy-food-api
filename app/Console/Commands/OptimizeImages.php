0c8cdabebced19b36e55ead99e9424369c32fa2f
Wern Ancheta <ancheta.wern@gmail.com>
2024-08-25 16:11:47 +0800

    [feature] add command for optimizing images

---
+++ app/Console/Commands/OptimizeImages.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\ImageOptimizer\OptimizerChainFactory;
+use Spatie\Image\Image;
+
+
+class OptimizeImages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:optimize-images {filename}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Optimizes uploaded images';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $filename = $this->argument('filename');
+        $pathToImage = public_path('storage/' . $filename);
+        $optimizerChain = OptimizerChainFactory::create();
+        $optimizerChain->optimize($pathToImage);
+        /*
+        Image::load($pathToImage)
+            ->width(640)
+            ->save();
+        */
+
+    }
+}
